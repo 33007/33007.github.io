5b572a9bc25021d4089c236ed2bbfbe35ddd429b
33007 <sun33007@hotmail.com>
2016-04-07 15:21:20 +0800

    Update timtest.js

--- timtest.js
+++ timtest.js
@@ -1,7 +1,7 @@
 function butt(){
 	document.write("<form class='formclass' name=autof style='padding:0px; margin:0px;'>")
 	for(var i=1;i<autourl.length;i++){
-		document.write("<div class='tline'><input type=text class='msinput' name=txt"+i+" size=10 value=测试中……><img src='images/arrow.png' border='0' /><input class='urlinput' type=text name=url"+i+" size=40>&nbsp;<input type='button'  onclick='window.open(this.form.url"+i+".value)' class='buttonopen'></div>")
+		document.write("<div class='tline'><input type=text class='msinput' name=txt"+i+" size=10 value=测试中……><img src='http://103.17.118.203:809/images/arrow.png' border='0' /><input class='urlinput' type=text name=url"+i+" size=40>&nbsp;<input type='button'  onclick='window.open(this.form.url"+i+".value)' class='buttonopen'></div>")
 	}
 	document.write("<center><input type=submit value=点击这里重新检测刷新访问速度 class='buttonsubmit'></center></form>")
 }
